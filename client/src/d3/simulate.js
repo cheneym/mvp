21c142e1bd5bd85b9411088fc2d93fba765c1749
Cheney Mao <cheneymao94@gmail.com>
2016-10-17 17:00:57 -0700

    Get robot to move according to data

--- client/src/d3/simulate.js
+++ client/src/d3/simulate.js
@@ -3,8 +3,6 @@ var d3 = require('d3');
 var render = function(data) {
   var h = window.innerHeight;
   var w = window.innerWidth;
-  var randomX = function() { return Math.random () * w; };
-  var randomY = function() { return Math.random () * h; };
 
   var container = d3
     .select('body')
@@ -29,30 +27,53 @@ var render = function(data) {
     .attr('r', robotRadius)
     .attr('fill', 'blue');
 
-  var particle = container
-    .selectAll('.particle')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('class', 'particle')
-    .attr('cx', d => d.position)
-    .attr('cy', +robot.attr('cy'))
-    .attr('r', particleRadius);
+  var robotMove = (element, index) => {
+    element.transition().duration(50).ease(d3.easeLinear)
+      .attr('cx', data[index].position)
+      .on('end', () => {
+        robotMove(d3.select('.robot'), index + 1);
+      });
+  }
+  robotMove(d3.select('.robot'), 0);
+  // var particle = container
+  //   .selectAll('.particle')
+  //   .data(data)
+  //   .enter()
+  //   .append('circle')
+  //   .attr('class', 'particle')
+  //   .attr('cx', d => d.position)
+  //   .attr('cy', +robot.attr('cy'))
+  //   .attr('r', particleRadius);
 
-  container.transition()
-      .duration(totalTime)
-      .ease(d3.easeLinear)
-      .selectAll('.robot')
-      .attr('cx', finalPos)
-      .attr('cy', h/2);
+  // container.transition()
+  //     .duration(totalTime)
+  //     .ease(d3.easeLinear)
+  //     .selectAll('.robot')
+  //     .attr('cx', finalPos)
+  //     .attr('cy', h/2);
 
-  transition = container.transition().duration(100);
-  var delay = (d, i) => i * 50;
-  console.log(typeof robot.attr('cx'));
-  transition.selectAll(".particle")
-      .delay(delay)
-      .attr('cx', d => d.position)
-      .attr('cy', d => +robot.attr('cy') + d.distance);
+  // transition = container.transition().duration(100);
+  // var delay = (d, i) => i * 50;
+  // console.log(typeof robot.attr('cx'));
+  // transition.selectAll(".particle")
+  //     .delay(delay)
+  //     .attr('cx', d => d.position)
+  //     .attr('cy', d => +robot.attr('cy') + d.distance);
+
+  // var positionData = randomPositions(n);
+
+  //   var circles = svgContainer.selectAll('.enemy')
+  //                             .data(positionData);
+  //   var nodes = circles.data();
+  //   circles.transition().duration(timeDelay).attr('cx', function(d) { return d.x; })
+  //          .attr('cy', function(d) { return d.y; });
+  //   circles.enter()
+  //          .append('circle')
+  //          .attr('class', 'enemy')
+  //          .attr('cx', function(d) { return d.x; })
+  //          .attr('cy', function(d) { return d.y; })
+  //          .attr('r', function(d) { return r; });
+  //   circles.exit().remove();
 };
 
 d3.json('http://localhost:8000/data', render);
